a869e27e8086060c65bdc3c19fa735171db66283
Dan Fuller <dancfuller@gmail.com>
2025-12-10 13:11:32 -0700

    code clean-up; longer power cycle delay;

--- pingandtogglesmartoutlet.ps1
+++ pingandtogglesmartoutlet.ps1
@@ -1,10 +1,10 @@
-if (Test-Connection 192.168.1.10 -Quiet)
-{
+$PC = "192.168.1.10"
+$smartPlug = "192.168.1.11"
+$bootWait = 15
+
+if (-not (Test-Connection $linuxPC -Count 2 -Quiet)) {
+    Write-Host "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - PC frozen, power cycling..."
+    kasa --host $smartPlug --plug off
+    Start-Sleep -Seconds $bootWait
+    kasa --host $smartPlug --plug on
 }
-else
-{
-	kasa --host 192.168.1.11 --plug off
-	Start-Sleep -Seconds 5
-	kasa --host 192.168.1.11 --plug on
-}
-exit
